af5351bd9182fb03bf0f4a14c2ddb1cdcd7e655b
root <root@DESKTOP-J4JA867.localdomain>
2023-12-11 16:23:34 -0600

    added code for list problems

---
+++ python/two_d_lists.py
@@ -0,0 +1,54 @@
+# Given 2D list calculate the sum of diagonal elements.
+
+def diagonal_sum(matrix):
+    # TODO
+    sum = 0
+    i = 0
+    for row in matrix:
+        sum += row[i]
+        i += 1
+    return sum
+
+myList2D = [[1,2,3],[4,5,6],[7,8,9]] 
+ 
+print(diagonal_sum(myList2D)) # 15
+
+
+"""
+def diagonal_sum(matrix):
+    # Initialize the sum to 0
+    total = 0
+ 
+    # Iterate through the rows of the matrix
+    for i in range(len(matrix)):
+        # Add the diagonal element to the total sum
+        total += matrix[i][i]
+ 
+    return total
+
+Explanation:
+
+def diagonal_sum(matrix):
+Define a function called diagonal_sum that takes a 2D list (matrix) as its argument.
+
+total = 0
+Initialize a variable 'total' to store the sum of the diagonal elements. Set its initial value to 0.
+
+for i in range(len(matrix)):
+Start a for loop that iterates through the range of the length of the matrix (number of rows). 
+The index variable is 'i'.
+
+total += matrix[i][i]
+Add the value of the diagonal element at the current index to the 'total'. 
+The diagonal element is the one where the row and column indices are the same (i.e., matrix[i][i]).
+
+return total
+After the for loop is done, return the total sum of the diagonal elements.
+
+Time complexity:
+O(n), where n is the number of rows (or columns) in the matrix. The function iterates through the rows once.
+
+Space complexity:
+O(1), as the function only uses a single variable to store the sum (total).
+
+"""
