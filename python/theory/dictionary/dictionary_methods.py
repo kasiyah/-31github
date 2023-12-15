518eaed93bc75e284aacd75823462de7a784ccf6
kasiyah <root@DESKTOP-J4JA867.localdomain>
2023-12-15 16:55:46 -0600

    small addition

--- python/theory/dictionary/dictionary_methods.py
+++ python/theory/dictionary/dictionary_methods.py
@@ -10,3 +10,8 @@ print(myDict)
 # copy() 
 # returns a shallow copy of dictionary. It does not modify the original dictionary.
 # when the method is called a new dictionary is created
+# syncdax: dictionary.copy()
+myDict2 = {'name': 'Edy', 'age': 26, 'address': 'London', 'education': 'master'}
+dict = myDict2.copy()
+print(myDict2)
+print(dict)
